d20c660def2d6f013eb0dc63c8eac5529375f65d
lkoida <leonidkoida@gmail.com>
2025-12-02 11:53:07 +0200

    added details

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import {ref, onMounted} from 'vue'
-import {projects} from './projects'
+import { onMounted, ref } from 'vue'
+import MyDetails from '@/components/MyDetails.vue'
+import { projects } from './projects'
 
 const pr = ref<
   Array<{
@@ -13,7 +14,7 @@ const pr = ref<
 >()
 
 onMounted(() => {
-  if (!localStorage.getItem('pr')){
+  if (!localStorage.getItem('pr')) {
     localStorage.setItem('pr', JSON.stringify(projects))
     pr.value = projects
   } else {
@@ -22,30 +23,25 @@ onMounted(() => {
 })
 
 const difficultyMap = {
-  'easy': `★☆☆☆`,
-  "medium": "★★☆☆",
-  'hard': '★★★☆',
+  easy: `★☆☆☆`,
+  medium: '★★☆☆',
+  hard: '★★★☆',
   'very-hard': '★★★★',
 }
-
 </script>
 
 <template>
   <h1>50 tiny projects on vue js</h1>
-  <ul>
-    <li v-for="project of pr" :key="project.id">
-      <h3 class="title">
-        {{ project.title }} <span class="level">{{ difficultyMap[project.difficulty] }}</span>
-      </h3>
-      <p class="description">
-        {{ project.description }}
-      </p>
-
-      <ul class="testing">
-        <li v-for="consideration in project.testing" :key="consideration">{{consideration}}</li>
-      </ul>
-    </li>
-  </ul>
+  <MyDetails
+    v-for="project in pr"
+    :key="project.id"
+    :id="project.id"
+    name="50-tiny-projects"
+    :title="project.title"
+    :difficulty="project.difficulty"
+    :description="project.description"
+    :testing="project.testing"
+  />
 </template>
 
 <style scoped></style>
---
+++ src/components/MyDetails.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{
+  id: number
+  title: string
+  difficulty: string
+  description: string
+  testing: Array<string>
+}>()
+</script>
+<template>
+  <details>
+    <summary>{{props.title}} {{props.difficulty}}</summary>
+    <p class="description">{{props.description}}</p>
+    <ul class="testing">
+      <li v-for="test in testing" :key="test">{{test}}</li>
+    </ul>
+  </details>
+</template>
+
+<style scoped></style>
