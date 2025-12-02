a280762a91ae4f910af318b14c0b1349f1a2fe4f
lkoida <leonidkoida@gmail.com>
2025-12-02 11:18:53 +0200

    created simple list for projects to host on github pages

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
-<script setup></script>
+<script setup lang="ts">
+import {ref, onMounted} from 'vue'
+import {projects} from './projects'
+
+const pr = ref<
+  Array<{
+    id: number
+    title: string
+    difficulty: string
+    description: string
+    testing: Array<string>
+  }>
+>()
+
+onMounted(() => {
+  if (!localStorage.getItem('pr')){
+    localStorage.setItem('pr', JSON.stringify(projects))
+    pr.value = projects
+  } else {
+    pr.value = JSON.parse(localStorage.getItem('pr'))
+  }
+})
+
+const difficultyMap = {
+  'easy': `★☆☆☆`,
+  "medium": "★★☆☆",
+  'hard': '★★★☆',
+  'very-hard': '★★★★',
+}
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>50 tiny projects on vue js</h1>
+  <ul>
+    <li v-for="project of pr" :key="project.id">
+      <h3 class="title">
+        {{ project.title }} <span class="level">{{ difficultyMap[project.difficulty] }}</span>
+      </h3>
+      <p class="description">
+        {{ project.description }}
+      </p>
+
+      <ul class="testing">
+        <li v-for="consideration in project.testing" :key="consideration">{{consideration}}</li>
+      </ul>
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
