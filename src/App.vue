951741cf217396acea810a990a1c74758e715a79
lkoida <leonidkoida@gmail.com>
2025-12-02 15:29:13 +0200

    styled a bit

--- src/App.vue
+++ src/App.vue
@@ -3,45 +3,56 @@ import { onMounted, ref } from 'vue'
 import MyDetails from '@/components/MyDetails.vue'
 import { projects } from './projects'
 
-const pr = ref<
-  Array<{
-    id: number
-    title: string
-    difficulty: string
-    description: string
-    testing: Array<string>
-  }>
->()
+export type ProjectType = {
+  id: number
+  title: string
+  difficulty: string
+  description: string
+  testing: Array<string>
+  isDone?: boolean
+}
+
+const pr = ref<Array<ProjectType>>()
+const complexityMap = {
+  easy: 0,
+  medium: 1,
+  hard: 2,
+  'very-hard': 3,
+}
+
+function compare(a: ProjectType, b: ProjectType) {
+  return complexityMap[a.difficulty] - complexityMap[b.difficulty]
+}
 
 onMounted(() => {
   if (!localStorage.getItem('pr')) {
     localStorage.setItem('pr', JSON.stringify(projects))
-    pr.value = projects
+    pr.value = projects.toSorted(compare)
   } else {
-    pr.value = JSON.parse(localStorage.getItem('pr'))
+    pr.value = JSON.parse(localStorage.getItem('pr')).toSorted(compare)
   }
 })
-
-const difficultyMap = {
-  easy: `★☆☆☆`,
-  medium: '★★☆☆',
-  hard: '★★★☆',
-  'very-hard': '★★★★',
-}
 </script>
 
 <template>
-  <h1>50 tiny projects on vue js</h1>
-  <MyDetails
-    v-for="project in pr"
-    :key="project.id"
-    :id="project.id"
-    name="50-tiny-projects"
-    :title="project.title"
-    :difficulty="project.difficulty"
-    :description="project.description"
-    :testing="project.testing"
-  />
+  <main>
+    <h1>50 tiny projects on vue js</h1>
+    <MyDetails
+      v-for="project in pr"
+      :key="project.id"
+      :id="project.id"
+      name="50-tiny-projects"
+      :title="project.title"
+      :difficulty="project.difficulty"
+      :description="project.description"
+      :testing="project.testing"
+    />
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  margin-inline: auto;
+  max-width: 50vw;
+}
+</style>
--- src/components/MyDetails.vue
+++ src/components/MyDetails.vue
@@ -6,15 +6,35 @@ const props = defineProps<{
   description: string
   testing: Array<string>
 }>()
+
+const isDone = defineModel('isDone')
+
+const difficultyMap = {
+  easy: `★☆☆☆`,
+  medium: '★★☆☆',
+  hard: '★★★☆',
+  'very-hard': '★★★★',
+}
 </script>
 <template>
-  <details>
-    <summary>{{props.title}} {{props.difficulty}}</summary>
-    <p class="description">{{props.description}}</p>
+  <details class="details">
+    <summary class="summary">
+      {{ props.title }} <span class="difficulty"> {{ difficultyMap[props.difficulty] }}</span>
+    </summary>
+    <p class="description">{{ props.description }}</p>
     <ul class="testing">
-      <li v-for="test in testing" :key="test">{{test}}</li>
+      <li v-for="test in testing" :key="test">{{ test }}</li>
     </ul>
+
   </details>
 </template>
 
-<style scoped></style>
+<style scoped>
+.details {
+  border-bottom: 1px solid black;
+  min-height: 48px;
+   & .summary {
+     cursor: pointer;
+   }
+}
+</style>
